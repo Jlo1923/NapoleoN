5d16dda5f19084cc94bbec19b7308110d8ef872a
Giussep Ricardo <giussepr@gmail.com>
2019-10-24 10:45:25 -0500

    Initial commit

---
+++ app/src/main/java/com/naposystems/pepito/ui/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.naposystems.pepito.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naposystems.pepito.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
