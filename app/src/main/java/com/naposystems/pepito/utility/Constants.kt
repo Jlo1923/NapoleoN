e5a04d33c567ea94cf2dfbc07b07dd620fae57ab
KraigYagami <kristian.naposystems@gmail.com>
2020-03-14 17:17:14 -0500

    fix issues

--- app/src/main/java/com/naposystems/pepito/utility/Constants.kt
+++ app/src/main/java/com/naposystems/pepito/utility/Constants.kt
@@ -12,10 +12,10 @@ object Constants {
     const val QUANTITY_ATTACHMENTS = 0
 
     object NapoleonApi {
-//        const val BASE_URL = "http://192.168.1.222/nn-backend-secret-chat/public/api/"
-        const val BASE_URL = "http://dev.napoleon-chat.com/api/"
-//        const val SOCKET_BASE_URL = "http://192.168.1.222:6001"
-        const val SOCKET_BASE_URL = "http://dev.napoleon-chat.com:6001"
+        const val BASE_URL = "http://192.168.1.222/nn-backend-secret-chat/public/api/"
+//        const val BASE_URL = "http://dev.napoleon-chat.com/api/"
+        const val SOCKET_BASE_URL = "http://192.168.1.222:6001"
+//        const val SOCKET_BASE_URL = "http://dev.napoleon-chat.com:6001"
         const val GENERATE_CODE = "auth/sendverificationcode"
         const val VERIFICATE_CODE = "auth/validateverificationcode"
         const val VALIDATE_NICKNAME = "auth/validatenick"
