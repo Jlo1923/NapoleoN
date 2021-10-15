34099fafdc57fac99f33edebd3b49cf3cc127f21
Elian Paz <=>
2021-10-15 08:19:54 -0500

    Fix NS-1206 complemento

--- app/src/main/java/com/naposystems/napoleonchat/di/module/general/PusherModule.kt
+++ app/src/main/java/com/naposystems/napoleonchat/di/module/general/PusherModule.kt
@@ -18,29 +18,36 @@ class PusherModule {
     @Provides
     @Singleton
     fun providePusher(sharedPreferencesManager: SharedPreferencesManager): Pusher {
-
-        val pusherOptions = PusherOptions()
-        pusherOptions.activityTimeout = 180000
-        pusherOptions.maxReconnectGapInSeconds = 4
-        pusherOptions.maxReconnectionAttempts = 3
-
-        val authorizer = HttpAuthorizer(BuildConfig.SOCKET_BASE_URL)
-
-        val mapAuth = HashMap<String, String>()
-
-        mapAuth["X-API-Key"] =
-            sharedPreferencesManager.getString(Constants.SharedPreferences.PREF_FIREBASE_ID, "")
-        authorizer.setHeaders(mapAuth)
-
-        pusherOptions.setCluster("us2")
-
-        pusherOptions.authorizer = authorizer
-
-        return Pusher(BuildConfig.PUSHER_KEY, pusherOptions)
+        return Pusher(BuildConfig.PUSHER_KEY, providePusherOptionsCreate(sharedPreferencesManager))
     }
 
     @Provides
     @Singleton
     fun provideSocket(): Socket = IO.socket(BuildConfig.SOCKET_BASE_URL)
+
+    companion object{
+        @Provides
+        fun providePusherOptionsCreate(sharedPreferencesManager: SharedPreferencesManager) : PusherOptions{
+            val pusherOptions = PusherOptions()
+            pusherOptions.activityTimeout = 180000
+            pusherOptions.maxReconnectGapInSeconds = 10
+            pusherOptions.maxReconnectionAttempts = 4
+
+            val authorizer = HttpAuthorizer(BuildConfig.SOCKET_BASE_URL)
+
+            val mapAuth = HashMap<String, String>()
+
+            mapAuth["X-API-Key"] =
+                sharedPreferencesManager.getString(Constants.SharedPreferences.PREF_FIREBASE_ID, "")
+            authorizer.setHeaders(mapAuth)
+
+            pusherOptions.setCluster("us2")
+
+            pusherOptions.authorizer = authorizer
+
+            return pusherOptions
+        }
+    }
+
 
 }
